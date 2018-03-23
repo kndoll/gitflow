47604f3a5a3c8b6f0d26d39fb1635d1d4520d358
kndoll <kndoll@DESKTOP-FNQIMLD>
2018-03-23 13:18:30 +0900

    init

---
+++ gitflow/src/main/java/com/gitflow/GitflowApplication.java
@@ -0,0 +1,12 @@
+package com.gitflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitflowApplication.class, args);
+	}
+}
